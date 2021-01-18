de4e9347a4ab47c5626098b165cf17a55d57daec
Stevenson Nelson <sonson521@gmail.com>
2021-01-17 23:31:55 -0500

    add event listener to button.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+const user = document.querySelector('#user');
+const password = document.querySelector('#password');
+const button = document.querySelector('.btn');
 
+button.addEventListener('click', submitBtn);
+
+function submitBtn(e) {
+  if (user.value === '') {
+    const div = document.createElement('div');
+    div.className = 'alert alert-info';
+    div.appendChild(document.createTextNode('You need words!!'));
+    const container = document.querySelector('.container');
+    container.insertBefore(div, container.childNodes[0]);
+    console.log(div);
+  } else {
+    console.log(user.value);
+  }
+  e.preventDefault();
+}
