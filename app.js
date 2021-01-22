9bb42d8ad9aac4d9ee17a275450520c269b0d55d
Stevenson Nelson <sonson521@gmail.com>
2021-01-22 13:43:19 -0500

    removed second div that was unnecessary

--- app.js
+++ app.js
@@ -18,33 +18,43 @@ function submitBtn(e) {
     console.log(div);
   }
   // when you validate the user input with "user"
-  if (user.value === 'user') {
-    const div2 = document.createElement('div');
-    div2.className = 'alert alert-success';
-    div2.appendChild(document.createTextNode('Hello User'));
-    setTimeout(() => {
-      div2.remove();
-    }, 4000);
-    const container = document.querySelector('.container');
+  else if (user.value === 'user' && password.value.length < 6) {
+    // const div2 = document.createElement('div');
+    // div2.className = 'alert alert-success';
+    // div2.appendChild(document.createTextNode('Hello User'));
+    // setTimeout(() => {
+    //   div2.remove();
+    // }, 4000);
+    // const container = document.querySelector('.container');
 
-    container.insertBefore(div2, container.childNodes[0]);
-    console.log(div2);
-  }
+    // container.insertBefore(div2, container.childNodes[0]);
 
-  // make password input more than six words
-  if (password.value.length <= 6) {
     // create div
     const div3 = document.createElement('div');
     const message = 'Must be at least 6 letters.';
-    div3.className = 'valid-feedback';
+    div3.className = 'invalid-feedback';
     div3.textContent = message;
-    setTimeout(() => {
-      div3.remove();
-    }, 3000);
     password.insertAdjacentHTML('afterend', message);
+
+    setTimeout(() => {
+      div3.className = 'valid-feedback';
+    }, 3000);
     console.log(div3);
-  } else {
-    console.log('Logged In!');
+    // console.log(div2);
+  }
+
+  // make password input more than six words
+  else {
+    const congrats = document.createElement('div');
+    congrats.className = 'alert alert-success';
+    const loggedInMessage = 'Welcome User';
+    congrats.appendChild(document.createTextNode(loggedInMessage));
+    const container = document.querySelector('.container');
+    container.insertBefore(congrats, container.childNodes[0]);
+    setTimeout(() => {
+      congrats.remove();
+    }, 3000);
+    console.log(congrats);
   }
   e.preventDefault();
 }
